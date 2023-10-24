2f46c05c464b17021b2a0233711e6c6380c684c6
yila-wen <jhuang4836@bths.edu>
2023-10-24 14:57:53 -0400

    Co-authored-by: LiquanCool <LiquanCool@users.noreply.github.com>

---
+++ c.js
@@ -0,0 +1,28 @@
+let counter = 0;
+const imgs = [questionMark.png, p1.jpg,];
+const img1 = document.getElementById("imgOne");
+initialize()
+{
+    
+}
+//first we have array of 8 images, we assign images to 2 button numbers
+//use the two variables above to keep track of what is selected
+//bobom banan doom boom easy  peasy
+initialize()
+{
+    
+}
+
+
+cardTurn(number)
+{
+if (counter <= 1){
+    img1
+
+}
+
+}
+
+imgChange(imgID){
+
+}
