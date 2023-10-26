b92d6e8a74f258c50b9ebeae790d0b2473b8cc8c
yila-wen <jhuang4836@bths.edu>
2023-10-26 15:30:13 -0400

    I think button works!! Only 2 hours

--- c.js
+++ c.js
@@ -1,28 +1,24 @@
+
+console.log("working");
 let counter = 0;
-const imgs = [questionMark.png, p1.jpg,];
-const img1 = document.getElementById("imgOne");
-initialize()
-{
-    
-}
+const img1 = document.getElementById("buttonOne");
+const img2 = document.getElementById("imgTwo");
+let imgID = "";
+let index = 0;
+
+console.log("working");
+
+
+
 //first we have array of 8 images, we assign images to 2 button numbers
 //use the two variables above to keep track of what is selected
 //bobom banan doom boom easy  peasy
-initialize()
-{
-    
-}
 
 
-cardTurn(number)
-{
-if (counter <= 1){
-    img1
 
-}
 
-}
-
-imgChange(imgID){
-
+function imgChange(num){   
+   
+img.src = "p" + num +".jpg"; 
+   
 }
